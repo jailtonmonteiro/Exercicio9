4cff3e077e80eb1d1b0359aa8d44b57890cd1e95
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-16 14:56:15 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio9/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+ordem crescente ou decrescente. */
+
+using System;
+
+namespace Exercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA, numberB;
+
+            Console.WriteLine("DIGITE VALOR A e B");
+            string[] value = Console.ReadLine().Split(' ');
+
+            numberA = int.Parse(value[0]);
+            numberB = int.Parse(value[1]);
+
+            if (numberA % numberB == 0 || numberB % numberA == 0)
+            {
+                Console.WriteLine("SÃO MULTIPLOS");
+            }
+            else
+            {
+                Console.WriteLine("NÃO SÃO MULTIPLOS");
+            }
+        }
+    }
+}
